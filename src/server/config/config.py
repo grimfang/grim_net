166c636f824a227398a4806d305e9bcda8fb0a24
Manfred Wolf <manfred.wolf@cosmokey.com>
2015-08-24 20:27:53 +0200

    Changed Config file from cfg to prc file
    Removed old config file (New prc file will be written after WritePRCFile function got called)
    New Features:
    - Old config doesn't gets overwritten so user configurations won't get lost

--- src/server/config/config.py
+++ src/server/config/config.py
@@ -2,44 +2,70 @@
 #----------------------------------------------------------------------#
 
 ## IMPORTS ##
-import ConfigParser
+import os
+from panda3d.core import (
+    ConfigVariableInt,
+    ConfigVariableString,
+    ConfigPageManager,
+    OFileStream,
+    loadPrcFile,
+    Filename
+)
 
 ########################################################################
 
+prcFile = os.path.join("..", "server.prc")
 
 class Config():
-    
+
     def __init__(self):
+        # if exist load the config file
+        if os.path.exists(prcFile):
+            loadPrcFile(Filename.fromOsSpecific(prcFile))
+        # set the variables using the config files content or set a default value
+        self.MOTD = ConfigVariableString('motd', 'Welcome to grim-net!').getValue()
+        self.HOSTNAME = ConfigVariableString('hostname', '127.0.0.1').getValue()
+        self.TCPPORT = ConfigVariableInt('tcp_port', '6000').getValue()
+        self.BACKLOG = ConfigVariableInt('backlog', '10').getValue()
+        self.UDPPORT = ConfigVariableInt('udp_port', '6001').getValue()
 
-        config = ConfigParser.RawConfigParser()
-        config.read('./server.cfg')
+    def WritePRCFile(self):
+        page = None
+        customConfigVariables = ["", "motd", "hostname", "tcp_port", "backlog", "udp_port"]
+        if os.path.exists(prcFile):
+            # load the existing config file
+            page = loadPrcFile(Filename.fromOsSpecific(prcFile))
+            removeDecls = []
+            for dec in range(page.getNumDeclarations()):
+                # Check if our variables are given.
+                # NOTE: This check has to be done to not loose our base or other
+                #       manual config changes by the user
+                if page.getVariableName(dec) in customConfigVariables:
+                    decl = page.modifyDeclaration(dec)
+                    removeDecls.append(decl)
+            for dec in removeDecls:
+                page.deleteDeclaration(dec)
+        else:
+            # create a new config file
+            cpMgr = ConfigPageManager.getGlobalPtr()
+            page = cpMgr.makeExplicitPage("Grim Net Pandaconfig")
 
-        self.MOTD = config.get('MOTD', 'motd')
-        self.HOSTNAME = config.get('NETWORK', 'hostname')
-        self.TCPPORT = config.getint('NETWORK', 'tcp_port')
-        self.BACKLOG = config.getint('NETWORK', 'backlog')
-        self.UDPPORT = config.getint('NETWORK', 'udp_port')
-        
-        #self.writeNewCfgFile()
+        # config declarations
+        page.makeDeclaration("motd", str(self.MOTD))
+        page.makeDeclaration("hostname", str(self.HOSTNAME))
+        page.makeDeclaration("tcp_port", str(self.TCPPORT))
+        page.makeDeclaration("backlog", str(self.BACKLOG))
+        page.makeDeclaration("udp_port", str(self.UDPPORT))
+
+        # create a stream to the specified config file
+        configfile = OFileStream(prcFile)
+        # and now write it out
+        page.write(configfile)
+        # close the stream
+        configfile.close()
 
 
-    def writeNewCfgFile(self, _data=[]):
-
-        config = ConfigParser.RawConfigParser()
-
-        config.add_section('MOTD')
-        config.set('MOTD', 'motd', 'Default')
-
-        config.add_section('NETWORK')
-        config.set('NETWORK', 'hostname', '127.0.0.1')
-        config.set('NETWORK', 'tcp_port', '6000')
-        config.set('NETWORK', 'backlog', '10')
-        config.set('NETWORK', 'udp_port', '6001')
-
-        with open('../server.cfg', 'wb') as cfg:
-            config.write(cfg)
-
-
-
+#s = Config()
+#s.WritePRCFile()
 # Writing & Testing
 #s = Config()
